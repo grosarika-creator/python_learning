b35c9394fa5f329614750c7c9b5d49f13d50500e
georgiinna <grosarika@gmail.com>
2026-09-14 17:34:11 +0800

    learn start triangle pattern

---
+++ lesson 5/star_triangle.py
@@ -0,0 +1,13 @@
+# *
+# * *
+# * * *
+# * * * *
+# * * * * *
+
+for i in range(10):
+    for j in range(i + 1):
+        print("* ", end="")
+    print()
+
+for i in range(1, 11):
+    print("* " * i)
