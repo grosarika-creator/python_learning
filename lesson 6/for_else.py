5f33304bb482fc1d6061669e65b4b9059ed89270
georgiinna <grosarika@gmail.com>
2026-09-18 21:45:29 +0800

    save for-else

--- lesson 6/for_else.py
+++ lesson 6/for_else.py
@@ -1,14 +1,14 @@
-numbers = [2, 4, 6, 8, 10]
+numbers = [2, 4, 6, 5, 10]
 
 # check for odd number on a list
 # normal code
-is_check = False
+is_odd = False
 for i in numbers:
     if i % 2 != 0:
-        is_check = True
+        is_odd = True
         odd = i
         break
-if is_check:
+if is_odd:
     print("Odd number:", odd)
 else:
     print("There's no odd number")
